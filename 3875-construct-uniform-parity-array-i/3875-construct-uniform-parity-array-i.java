f653892aa670086af60b209ef4233e3dd7b9fe55
Suyash Rathod <suyashr84@gmail.com>
2026-09-02 14:35:50 +0530

    Time: 0 ms (100%), Space: 45 MB (87.6%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        // Because of the mathematical properties of parity,
+        // it is always possible to construct the required array.
+        return true;
+    }
+}
